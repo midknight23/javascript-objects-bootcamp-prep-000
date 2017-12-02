733167d0c67fe53ce4dc94cdafb4c3d5b027c9f1
Robert Fischer <lilbfisch@gmail.com>
2017-12-02 21:36:23 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {BrunoMars: 'Grenade'};
+
+function updatePlaylist(playlist, {artist, song} ) {
+  Object.assign({}, playlist, {artist: song})
+  return playlist
+}
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.artist
+  return playlist
+}
